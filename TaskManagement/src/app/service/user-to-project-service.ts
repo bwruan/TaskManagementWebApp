598604c4b6086562ad04bc60b3389d786f10450e
Becky <becky.ruan524@gmail.com>
2021-06-28 16:29:48 -0700

    Finished add member

--- TaskManagement/src/app/service/user-to-project-service.ts
+++ TaskManagement/src/app/service/user-to-project-service.ts
@@ -4,6 +4,7 @@ import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
 import { Account } from "../model/account";
 import { Project } from "../model/project";
+import { MemberRequest } from "../model/request/member-request";
 
 @Injectable({
     providedIn: 'root'
@@ -31,5 +32,14 @@ export class UserToProjectService {
 
         let accountId = localStorage.getItem("accountId");
         return this._http.get<Project[]>(this.baseUrl + "/usertoproject/account/" + accountId, {headers: header});
+    }
+
+    addMember(memberObj: MemberRequest): Observable<Account>{
+        let token = localStorage.getItem("token");
+        let header = new HttpHeaders({
+            "Authorization": "Bearer "+ token
+        });
+
+        return this._http.patch<Account>(this.baseUrl + "/usertoproject/addMember", memberObj, {headers: header});
     }
 }
