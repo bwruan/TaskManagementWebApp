9862580e25419f1e8e46fcf3858836fd966004c7
Becky <becky.ruan524@gmail.com>
2021-08-15 10:13:09 -0700

    editing home/landing page

--- TaskManagement/src/app/app-routing.module.ts
+++ TaskManagement/src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ProjectPageComponent } from './project/project-page/project-page.component';
 import { ProjectComponent } from './project/project.component';
 import { UpdateAccountComponent } from './update-account/update-account.component';
 
 const routes: Routes = [
+  {path: "", component: LandingPageComponent},
   {path: "accountsetting", component: UpdateAccountComponent},
   {path: "projects", component: ProjectComponent},
-  {path: "projects/:id", component: ProjectPageComponent}
-  // {path: '**', component: NotFoundComponent}
+  {path: "projects/:id", component: ProjectPageComponent},
+  {path: "**", component: NotFoundComponent}
 ];
 
 @NgModule({
